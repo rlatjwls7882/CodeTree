2e7e3e38967cc7d6a4664738f158d483ae1bf04a
Seojin Kim <rlatjwls7882@gmail.com>
2026-05-22 15:14:03 +0000

    위치 변경

--- print-in-variety.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cout << "Total days in Year\n365\nCircumference rate\n3.1415926535";
-}
